63032bc7fcfba6801299f8e7b673fbea1faa04cf
bonethugs41 <pavlov.martin@gmail.com>
2013-11-15 22:24:36 +0200

    04.Console_input_and_output
    Excercises 1 and 2

---
+++ 04.Console_input_and_output/01.Print_integers_sum/01.Print_integers_sum.cs
@@ -0,0 +1,21 @@
+﻿//Write a program that reads 3 integer numbers from the console and prints their sum.
+
+
+using System;
+
+class Summary
+{
+    static void Main()
+    {
+        Console.Title = "Summary";
+        Console.WriteLine("Please enter three random numbers" + "\nFirst number:");
+        int int1 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Second number:");
+        int int2 = int.Parse(Console.ReadLine());
+        Console.WriteLine("Third number:");
+        int int3 = int.Parse(Console.ReadLine());
+        int sum;
+        sum = int1 + int2 + int3;
+        Console.WriteLine("Sum = {0} ", sum);
+    }
+}
---
+++ 04.Console_input_and_output/02.Circle_perimeter_and_area/Program.cs
@@ -0,0 +1,20 @@
+﻿//Write a program that reads the radius r of a circle and prints its perimeter and area.
+
+
+
+using System;
+
+class CircleParameters
+{ 
+    static void Main()
+    {
+        Console.Title = "Perimeter and area of a circle";
+        Console.WriteLine("Please input the radius of the circle:");
+        double radius = double.Parse(Console.ReadLine());
+        double perimeter = 2 * radius * 3.14;
+        double area = 3.14 * radius * radius;
+        Console.WriteLine("The perimeter of the circle is: {0}", perimeter);
+        Console.WriteLine("The area of the circle is: {0}", area);
+
+    }
+}
