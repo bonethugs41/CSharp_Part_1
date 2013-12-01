d8d0d3c3d76ae963aa7e053e0ef580fbd13e0787
bonethugs41 <pavlov.martin@gmail.com>
2013-12-02 00:54:10 +0200

    07.Exam_preparation
    Tripple rotation of digits

---
+++ 7.Exam_preparation/03.Tripple_rotation_of_digits/Tripple_rotation_of_digits.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class TripleRotation
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+        for (int i = 1; i < 4; i++)
+        {
+            int lastDigit = number % 10;
+            number = number / 10;
+            string result1 = lastDigit.ToString() + number.ToString();
+            number = int.Parse(result1);
+        }
+        Console.WriteLine(number);
+    }
+}
